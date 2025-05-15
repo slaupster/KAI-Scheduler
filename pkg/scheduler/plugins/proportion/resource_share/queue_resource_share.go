daa6ca68a97a3e29028290370a18c56102223575
davidLif <davidshani12@gmail.com>
2025-05-15 12:03:42 +0300

    Cache GetDeservedShare and GetFairShare (#139)

--- pkg/scheduler/plugins/proportion/resource_share/queue_resource_share.go
+++ pkg/scheduler/plugins/proportion/resource_share/queue_resource_share.go
@@ -47,6 +47,10 @@ type QueueResourceShare struct {
 	CPU    ResourceShare
 	Memory ResourceShare
 	GPU    ResourceShare
+
+	// cache
+	lastDeservedShare ResourceQuantities
+	lastFairShare     ResourceQuantities
 }
 type resourceShareMapFunc func(rs *ResourceShare) float64
 
@@ -70,10 +74,13 @@ func (qrs *QueueResourceShare) GetAllocatableShare() ResourceQuantities {
 }
 
 func (qrs *QueueResourceShare) GetFairShare() ResourceQuantities {
-	f := func(rs *ResourceShare) float64 {
-		return rs.FairShare
+	if qrs.lastFairShare == nil {
+		f := func(rs *ResourceShare) float64 {
+			return rs.FairShare
+		}
+		qrs.lastFairShare = qrs.buildResourceQuantities(f)
 	}
-	return qrs.buildResourceQuantities(f)
+	return qrs.lastFairShare
 }
 
 func (qrs *QueueResourceShare) GetAllocatedShare() ResourceQuantities {
@@ -91,10 +98,13 @@ func (qrs *QueueResourceShare) GetAllocatedNonPreemptible() ResourceQuantities {
 }
 
 func (qrs *QueueResourceShare) GetDeservedShare() ResourceQuantities {
-	f := func(rs *ResourceShare) float64 {
-		return rs.Deserved
+	if qrs.lastDeservedShare == nil {
+		f := func(rs *ResourceShare) float64 {
+			return rs.Deserved
+		}
+		qrs.lastDeservedShare = qrs.buildResourceQuantities(f)
 	}
-	return qrs.buildResourceQuantities(f)
+	return qrs.lastDeservedShare
 }
 
 func (qrs *QueueResourceShare) GetMaxAllowedShare() ResourceQuantities {
@@ -156,6 +166,9 @@ func (qrs *QueueResourceShare) GetDominantResourceShare(totalResources ResourceQ
 func (qrs *QueueResourceShare) AddResourceShare(resource ResourceName, amount float64) {
 	resourceShare := qrs.ResourceShare(resource)
 	resourceShare.FairShare += amount
+
+	// invalidate fairshare cache
+	qrs.lastFairShare = nil
 }
 
 func (qrs *QueueResourceShare) SetQuotaResources(resource ResourceName, deserved float64, maxAllowed float64,
@@ -164,4 +177,7 @@ func (qrs *QueueResourceShare) SetQuotaResources(resource ResourceName, deserved
 	resourceShare.Deserved = deserved
 	resourceShare.MaxAllowed = maxAllowed
 	resourceShare.OverQuotaWeight = overQuotaWeight
+
+	// invalidate cache for deserved share
+	qrs.lastDeservedShare = nil
 }
