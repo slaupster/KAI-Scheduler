d59ab5eb6657956c7b8f37ab30b101254c106df4
davidLif <davidshani12@gmail.com>
2025-08-01 15:47:13 +0300

    Topology structs updates: (#352)
    1- Remove resources fields from TopologyDomainInfo
    2- Saves domains by level for TopologyInfo

--- pkg/scheduler/plugins/topology/topology_plugin.go
+++ pkg/scheduler/plugins/topology/topology_plugin.go
@@ -4,9 +4,10 @@
 package topology
 
 import (
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/common_info"
 	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/node_info"
-	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/pod_info"
 	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/framework"
+	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/k8s_internal"
 	kueuev1alpha1 "sigs.k8s.io/kueue/apis/kueue/v1alpha1"
 )
 
@@ -16,8 +17,11 @@ const (
 )
 
 type topologyPlugin struct {
-	enabled       bool
-	TopologyTrees map[string]*TopologyInfo
+	enabled            bool
+	taskOrderFunc      common_info.LessFn
+	sessionStateGetter k8s_internal.SessionStateProvider
+	nodesInfos         map[string]*node_info.NodeInfo
+	TopologyTrees      map[string]*TopologyInfo
 }
 
 func New(pluginArgs map[string]string) framework.Plugin {
@@ -33,19 +37,17 @@ func (t *topologyPlugin) Name() string {
 
 func (t *topologyPlugin) OnSessionOpen(ssn *framework.Session) {
 	topologies := ssn.Topologies
+	t.taskOrderFunc = ssn.TaskOrderFn
+	t.sessionStateGetter = ssn
+	t.nodesInfos = ssn.Nodes
 	t.initializeTopologyTree(topologies, ssn)
-
-	ssn.AddEventHandler(&framework.EventHandler{
-		AllocateFunc:   t.handleAllocate(ssn),
-		DeallocateFunc: t.handleDeallocate(ssn),
-	})
 }
 
 func (t *topologyPlugin) initializeTopologyTree(topologies []*kueuev1alpha1.Topology, ssn *framework.Session) {
 	for _, singleTopology := range topologies {
 		topologyTree := &TopologyInfo{
 			Name:             singleTopology.Name,
-			Domains:          map[TopologyDomainID]*TopologyDomainInfo{},
+			DomainsByLevel:   map[string]map[TopologyDomainID]*TopologyDomainInfo{},
 			Root:             NewTopologyDomainInfo(TopologyDomainID("root"), "datacenter", "cluster", 0),
 			TopologyResource: singleTopology,
 		}
@@ -69,10 +71,16 @@ func (*topologyPlugin) addNodeDataToTopology(topologyTree *TopologyInfo, singleT
 		}
 
 		domainId := calcDomainId(levelIndex, singleTopology.Spec.Levels, nodeInfo.Node.Labels)
-		domainInfo, foundLevelLabel := topologyTree.Domains[domainId]
+		domainLevel := level.NodeLabel
+		domainsForLevel, foundLevelLabel := topologyTree.DomainsByLevel[domainLevel]
 		if !foundLevelLabel {
+			topologyTree.DomainsByLevel[level.NodeLabel] = map[TopologyDomainID]*TopologyDomainInfo{}
+			domainsForLevel = topologyTree.DomainsByLevel[level.NodeLabel]
+		}
+		domainInfo, foundDomain := domainsForLevel[domainId]
+		if !foundDomain {
 			domainInfo = NewTopologyDomainInfo(domainId, domainName, level.NodeLabel, levelIndex+1)
-			topologyTree.Domains[domainId] = domainInfo
+			domainsForLevel[domainId] = domainInfo
 		}
 		domainInfo.AddNode(nodeInfo)
 
@@ -84,50 +92,6 @@ func (*topologyPlugin) addNodeDataToTopology(topologyTree *TopologyInfo, singleT
 	}
 	nodeContainingChildDomain.Parent = topologyTree.Root
 	topologyTree.Root.AddNode(nodeInfo)
-}
-
-func (t *topologyPlugin) handleAllocate(ssn *framework.Session) func(event *framework.Event) {
-	return t.updateTopologyGivenPodEvent(ssn, func(domainInfo *TopologyDomainInfo, podInfo *pod_info.PodInfo) {
-		domainInfo.AllocatedResources.AddResourceRequirements(podInfo.AcceptedResource)
-		domainInfo.AllocatedResources.BaseResource.ScalarResources()["pods"] =
-			domainInfo.AllocatedResources.BaseResource.ScalarResources()["pods"] + 1
-	})
-}
-
-func (t *topologyPlugin) handleDeallocate(ssn *framework.Session) func(event *framework.Event) {
-	return t.updateTopologyGivenPodEvent(ssn, func(domainInfo *TopologyDomainInfo, podInfo *pod_info.PodInfo) {
-		domainInfo.AllocatedResources.SubResourceRequirements(podInfo.AcceptedResource)
-		domainInfo.AllocatedResources.BaseResource.ScalarResources()["pods"] =
-			domainInfo.AllocatedResources.BaseResource.ScalarResources()["pods"] - 1
-	})
-}
-
-func (t *topologyPlugin) updateTopologyGivenPodEvent(
-	ssn *framework.Session,
-	domainUpdater func(domainInfo *TopologyDomainInfo, podInfo *pod_info.PodInfo),
-) func(event *framework.Event) {
-	return func(event *framework.Event) {
-		pod := event.Task.Pod
-		nodeName := event.Task.NodeName
-		if nodeName == noNodeName {
-			return
-		}
-		node := ssn.Nodes[nodeName].Node
-		podInfo := ssn.Nodes[nodeName].PodInfos[pod_info.PodKey(pod)]
-
-		for _, topologyTree := range t.TopologyTrees {
-			leafDomainId := calcLeafDomainId(topologyTree.TopologyResource, node.Labels)
-			domainInfo := topologyTree.Domains[leafDomainId]
-			for domainInfo != nil {
-				domainUpdater(domainInfo, podInfo)
-
-				if domainInfo.Nodes[nodeName] != nil {
-					break
-				}
-				domainInfo = domainInfo.Parent
-			}
-		}
-	}
 }
 
 func (t *topologyPlugin) OnSessionClose(ssn *framework.Session) {}
--- pkg/scheduler/plugins/topology/topology_structs.go
+++ pkg/scheduler/plugins/topology/topology_structs.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/node_info"
-	"github.com/NVIDIA/KAI-scheduler/pkg/scheduler/api/resource_info"
 	kueuev1alpha1 "sigs.k8s.io/kueue/apis/kueue/v1alpha1"
 )
 
@@ -19,8 +18,8 @@ type TopologyInfo struct {
 	// Root of the topology tree
 	Root *TopologyDomainInfo
 
-	// Map of all domains by their ID for quick lookup
-	Domains map[TopologyDomainID]*TopologyDomainInfo
+	// Map of all domains by their level for quick lookup
+	DomainsByLevel map[string]map[TopologyDomainID]*TopologyDomainInfo
 
 	// Name of this topology configuration
 	Name string
@@ -49,17 +48,8 @@ type TopologyDomainInfo struct {
 	// Nodes that belong to this domain
 	Nodes map[string]*node_info.NodeInfo
 
-	// Total available resources in this domain
-	AvailableResources *resource_info.Resource
-
-	// Total allocated resources in this domain
-	AllocatedResources *resource_info.Resource
-
 	// Number of pods that can be allocated in this domain for the job
 	AllocatablePods int
-
-	// List of resources requested by each pod in the job this tree is built for
-	RequestedResources *resource_info.ResourceRequirements
 
 	// Depth in the tree from root (0 for root)
 	Depth int
@@ -67,16 +57,13 @@ type TopologyDomainInfo struct {
 
 func NewTopologyDomainInfo(id TopologyDomainID, name, level string, depth int) *TopologyDomainInfo {
 	return &TopologyDomainInfo{
-		ID:                 id,
-		Name:               name,
-		Level:              level,
-		Parent:             nil,
-		Children:           []*TopologyDomainInfo{},
-		Nodes:              map[string]*node_info.NodeInfo{},
-		AvailableResources: resource_info.EmptyResource(),
-		AllocatedResources: resource_info.EmptyResource(),
-		RequestedResources: nil,
-		Depth:              depth,
+		ID:       id,
+		Name:     name,
+		Level:    level,
+		Parent:   nil,
+		Children: []*TopologyDomainInfo{},
+		Nodes:    map[string]*node_info.NodeInfo{},
+		Depth:    depth,
 	}
 }
 
@@ -93,15 +80,6 @@ func calcDomainId(leafLevelIndex int, levels []kueuev1alpha1.TopologyLevel, node
 	return TopologyDomainID(strings.Join(domainsNames, "."))
 }
 
-func calcLeafDomainId(topologyResource *kueuev1alpha1.Topology, nodeLabels map[string]string) TopologyDomainID {
-	return calcDomainId(len(topologyResource.Spec.Levels)-1, topologyResource.Spec.Levels, nodeLabels)
-}
-
 func (t *TopologyDomainInfo) AddNode(nodeInfo *node_info.NodeInfo) {
 	t.Nodes[nodeInfo.Node.Name] = nodeInfo
-	t.AvailableResources.Add(nodeInfo.Allocatable)
-	t.AllocatedResources.Add(nodeInfo.Used)
-	// TODO: do not take into account fractional gpus
-	t.AllocatedResources.BaseResource.ScalarResources()["pods"] =
-		t.AllocatedResources.BaseResource.ScalarResources()["pods"] + int64(len(nodeInfo.PodInfos))
 }
