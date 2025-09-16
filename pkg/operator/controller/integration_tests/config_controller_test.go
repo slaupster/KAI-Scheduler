12fbac3f87dd362ed6cb591368b0bfa8a355cf31
Erez Freiberger <enoodle@gmail.com>
2025-09-16 16:07:41 +0200

    adding operator integration tests (#510)

---
+++ pkg/operator/controller/integration_tests/config_controller_test.go
@@ -0,0 +1,119 @@
+// Copyright 2025 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
+package integration_tests
+
+import (
+	"context"
+	"os"
+
+	kaiv1 "github.com/NVIDIA/KAI-scheduler/pkg/apis/kai/v1"
+	kaiv1binder "github.com/NVIDIA/KAI-scheduler/pkg/apis/kai/v1/binder"
+	"github.com/NVIDIA/KAI-scheduler/pkg/common/constants"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	nvidiav1 "github.com/NVIDIA/gpu-operator/api/nvidia/v1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	v1common "github.com/NVIDIA/KAI-scheduler/pkg/apis/kai/v1/common"
+)
+
+const (
+	githubRoot = "../../../../"
+	repository = "ghcr.io/nvidia/kai-scheduler"
+	tag        = "latest"
+)
+
+var _ = Describe("KAIConfigController", Ordered, func() {
+	var (
+		kaiConfig *kaiv1.Config
+	)
+
+	BeforeAll(func() {
+		kaiConfig = &kaiv1.Config{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: constants.DefaultKAIConfigSingeltonInstanceName,
+			},
+			Spec: kaiv1.ConfigSpec{
+				Namespace: "kai-scheduler",
+				Binder: &kaiv1binder.Binder{
+					Service: &v1common.Service{
+						Enabled: ptr.To(true),
+					},
+				},
+			},
+		}
+		os.Setenv(v1common.DefaultRepositoryEnvVarName, repository)
+		os.Setenv(v1common.DefaultTagEnvVarName, tag)
+
+		Expect(k8sClient.Create(context.Background(), kaiConfig)).To(Succeed())
+	})
+
+	AfterAll(func() {
+		Expect(k8sClient.Delete(context.Background(), kaiConfig)).To(Succeed())
+		os.Unsetenv(v1common.DefaultRepositoryEnvVarName)
+		os.Unsetenv(v1common.DefaultTagEnvVarName)
+	})
+
+	Context("Watching ClusterPolicy", Ordered, func() {
+		It("Updates binder deployment when ClusterPolicy changes", func(ctx context.Context) {
+			var binderDeploymentGeneration int64
+			Eventually(func(g Gomega) {
+				binderDeployment := &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "binder",
+						Namespace: kaiConfig.Spec.Namespace,
+					},
+				}
+				g.Expect(k8sClient.Get(context.Background(), client.ObjectKeyFromObject(binderDeployment), binderDeployment)).To(Succeed())
+				binderDeploymentGeneration = binderDeployment.Generation
+			}, "10s", "200ms").Should(Succeed())
+
+			Expect(k8sClient.Create(ctx, &nvidiav1.ClusterPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "example-cluster-policy",
+				},
+				Spec: nvidiav1.ClusterPolicySpec{
+					Operator: nvidiav1.OperatorSpec{
+						DefaultRuntime: nvidiav1.Docker,
+					},
+					CDI: nvidiav1.CDIConfigSpec{
+						Enabled: ptr.To(true),
+						Default: ptr.To(true),
+					},
+				},
+			})).To(Succeed())
+
+			Eventually(func(g Gomega) bool {
+				binderDeployment := &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "binder",
+						Namespace: kaiConfig.Spec.Namespace,
+					},
+				}
+				g.Expect(k8sClient.Get(context.Background(), client.ObjectKeyFromObject(binderDeployment), binderDeployment)).To(Succeed())
+
+				return binderDeploymentGeneration < binderDeployment.Generation
+			}, "10s", "200ms").Should(BeTrue())
+
+			Eventually(func(g Gomega) bool {
+				updatedConfig := &kaiv1.Config{}
+				g.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: constants.DefaultKAIConfigSingeltonInstanceName}, updatedConfig)).To(Succeed())
+
+				for _, condition := range updatedConfig.Status.Conditions {
+					if condition.Type == string(kaiv1.ConditionTypeDeployed) && condition.Status == metav1.ConditionTrue {
+						return true
+					}
+				}
+				return false
+			}, "10s", "200ms").Should(BeTrue())
+		})
+	})
+
+})
---
+++ pkg/operator/controller/integration_tests/suite_test.go
@@ -0,0 +1,137 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package integration_tests
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	kaiv1 "github.com/NVIDIA/KAI-scheduler/pkg/apis/kai/v1"
+	"github.com/NVIDIA/KAI-scheduler/pkg/common/constants"
+	"github.com/NVIDIA/KAI-scheduler/pkg/operator/controller"
+
+	nvidiav1 "github.com/NVIDIA/gpu-operator/api/nvidia/v1"
+	admissionv1 "k8s.io/api/admissionregistration/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/apis/apiserver"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	// +kubebuilder:scaffold:imports
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var testContext context.Context
+var cancelTestContext context.CancelFunc
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
+var k8sManager ctrl.Manager
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecs(t, "Controller Suite")
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	testContext, cancelTestContext = context.WithCancel(context.Background())
+
+	scheme := runtime.NewScheme()
+	Expect(v1.AddToScheme(scheme)).To(Succeed())
+	Expect(appsv1.AddToScheme(scheme)).To(Succeed())
+	Expect(apiserver.AddToScheme(scheme)).To(Succeed())
+	Expect(admissionv1.AddToScheme(scheme)).To(Succeed())
+	Expect(apiextensionsv1.AddToScheme(scheme)).To(Succeed())
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "..", "..", "..", "deployments", "kai-scheduler", "crds"),
+			filepath.Join("..", "..", "..", "..", "deployments", "external-crds"),
+			"third_party_crds",
+		},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+	// cfg is defined in this file globally.
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	err = kaiv1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = nvidiav1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	// +kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	k8sManager, err = ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	configReconciler := &controller.ConfigReconciler{
+		Client: k8sManager.GetClient(),
+		Scheme: k8sManager.GetScheme(),
+	}
+	configReconciler.SetOperands(controller.ConfigReconcilerOperands)
+	err = configReconciler.SetupWithManager(k8sManager)
+	Expect(err).NotTo(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(testContext)
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
+	for _, namespace := range []string{"kai-scheduler", constants.DefaultResourceReservationName, constants.DefaultScaleAdjustName} {
+		kaiNamespace := &v1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: namespace,
+			},
+		}
+		Expect(k8sClient.Create(context.Background(), kaiNamespace)).To(Succeed())
+	}
+})
+
+var _ = AfterSuite(func() {
+	cancelTestContext()
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+})
